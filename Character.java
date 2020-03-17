9661712109005c838ff1b916e4ea966cdd3144b9
yyu8k <yyu8k@icloud.com>
2020-03-17 20:47:32 +0900

    Update Character.java

--- Character.java
+++ Character.java
@@ -5,13 +5,23 @@ public class Character {
   private int mp;
   private String name;
 
-  Character(int hp, int mp, String name) {
+  public Character(int hp, int mp, String name) {
     this.hp = hp;
     this.mp = mp;
     this.name = name;
   }
 
-  void introduce() {
+  public void introduce() {
     System.out.println("私の名前は"+name+"です。体力は"+hp+"持っていて、魔力は"+mp+"持っています。");
+  }
+
+  public void damage(int power) {
+    hp -= power;
+    System.out.println(name+"は"+power+"のダメージを受けました。");
+  }
+
+  public void attack(Character target) {
+    System.out.println(name+"の攻撃！");
+    target.damage(10);
   }
 }
