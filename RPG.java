cc63d954c628bbe024adaf1e8f2702a503d5725b
yyu8k <yyu8k@icloud.com>
2020-04-09 20:17:41 +0900

    Update RPG.java

--- RPG.java
+++ RPG.java
@@ -1,15 +1,15 @@
 package homeworks;
 
 public class RPG {
-	public static void main(String[] args) {
-		Character a = new Character();
-	    Dog b = new Dog();
-	    Character c = new Cat();
-	    a.introduce();
-	    b.introduce();
-	    c.introduce();
-	    a.attack(b);
-	    b.attack(c);
-	    c.attack(a);
-	  }
+  public static void main(String[] args) {
+    Character a = new Character();
+    Dog b = new Dog();
+    Character c = new Cat();
+    a.introduce();
+    b.introduce();
+    c.introduce();
+    a.attack(b);
+    b.attack(c);
+    c.attack(a);
+  }
 }
